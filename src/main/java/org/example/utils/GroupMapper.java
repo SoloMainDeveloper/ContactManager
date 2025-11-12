a22a6eb0f47421d676abe747446298e6f28a73a2
Alexander Govorukhin <alexandergdev20@gmail.com>
2025-11-12 18:08:22 +0500

    Задача 2. Реализован репозиторий групп

---
+++ src/main/java/org/example/utils/GroupMapper.java
@@ -0,0 +1,26 @@
+package org.example.utils;
+
+import org.example.entity.Group;
+
+import java.sql.ResultSet;
+
+/**
+ * Маппер данных из БД в сущность Группы
+ */
+public class GroupMapper {
+    /**
+     * Создаёт группу на основе ответа от БД
+     */
+    public Group resultSetToGroupEntity(ResultSet resultSet) {
+        GroupConverter converter = new GroupConverter();
+        try {
+            return new Group(
+                    resultSet.getLong("chat_id"),
+                    resultSet.getString("name"),
+                    converter.stringToContactIds(resultSet.getString("contact_ids"))
+            );
+        } catch (Exception exception) {
+            return null;
+        }
+    }
+}
