665e3a367aace62eb9d755e6ee8abed60b69e92a
Михаил Соломеин <solomein-m@mail.ru>
2025-11-24 17:38:24 +0500

    Задача 1. JavaDoc для созданных классов исключений

--- src/main/java/org/example/exceptions/ContactDoesNotExistException.java
+++ src/main/java/org/example/exceptions/ContactDoesNotExistException.java
@@ -1,6 +1,12 @@
 package org.example.exceptions;
 
+/**
+ * Исключение: контакт не существует
+ */
 public class ContactDoesNotExistException extends Exception {
+    /**
+     * Конструктор
+     */
     public ContactDoesNotExistException(String message) {
         super(message);
     }
